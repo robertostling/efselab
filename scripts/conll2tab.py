af85ddc45114bda94248c130e36f9d733590c2f7
Robert Östling <robert@ling.su.se>
2016-05-08 12:17:19 +0200

    conll2tab.py script can now handle named entity tags from SUC.

--- scripts/conll2tab.py
+++ scripts/conll2tab.py
@@ -5,14 +5,30 @@
 #
 # cut -f 2,4 file.conll >file.tab
 
+"""
+cat /home/corpora/SUC3.0/corpus/conll/blogs.conll /home/corpora/SUC3.0/corpus/conll/suc-train.conll | python3 conll2tab.py ne >../suc-data/suc-blogs-ne-train.tab
+cat /home/corpora/SUC3.0/corpus/conll/suc-dev.conll | python3 conll2tab.py ne >../suc-data/suc-ne-dev.tab
+cat /home/corpora/SUC3.0/corpus/conll/suc-train.conll | python3 conll2tab.py ne >../suc-data/suc-ne-train.tab
+"""
+
 import sys
+
+include_ne = 'ne' in sys.argv[1:]
 
 for line in sys.stdin:
     fields = line.rstrip('\n').split('\t')
     if len(fields) >= 6:
         word = fields[1]
-        tag = fields[3]+'|'+fields[5] if (fields[5] and fields[5] != '_') else fields[3]
-        print(word+'\t'+tag)
+        pos = fields[3]
+        if pos == 'LE': pos = 'IN'
+        tag = pos+'|'+fields[5] if (fields[5] and fields[5] != '_') else pos
+        if include_ne and len(fields) >= 12:
+            ne = fields[10] if fields[11] == '_' else (
+                    '%s-%s' % (fields[10], fields[11]))
+            lemma = fields[2]
+            print(word+'\t'+lemma+'\t'+tag+'\t'+ne)
+        else:
+            print(word+'\t'+tag)
     else:
         print()
 
