e16fa790f1791152f4ad7cabbfcb7861eddedb15
Emil Stenström <em@kth.se>
2016-06-11 18:29:47 +0200

    Add positive tests.

--- tests/test_commandline.py
+++ tests/test_commandline.py
@@ -1,10 +1,14 @@
 import unittest
-from commandline import create_parser, validate_options, ERROR_MESSAGES
+from commandline import (
+    create_parser, validate_options, ERROR_MESSAGES,
+    TAGGING_MODEL, NER_MODEL, LEMMATIZATION_MODEL, PARSING_MODEL, MALT,
+)
 
 def _validate_args(args):
     parser = create_parser()
     options, args = parser.parse_args(args)
     validate_options(options, args)
+    return options, args
 
 class TestCommandlineFailiures(unittest.TestCase):
     def test_empty(self):
@@ -70,3 +74,34 @@ class TestCommandlineFailiures(unittest.TestCase):
             _validate_args(["--parsed", "--output=DIR", "--parsing-model=MODEL", "out.txt"])
 
         self.assertEqual(str(manager.exception), ERROR_MESSAGES.not_found_parsing_model % "MODEL.mco")
+
+class TestCommandlineSuccesses(unittest.TestCase):
+    def test_all(self):
+        options, args = _validate_args(["--all", "--output=DIR", "out.txt"])
+        self.assertEqual(options.tokenized, True)
+        self.assertEqual(options.tagged, True)
+        self.assertEqual(options.lemmatized, True)
+        self.assertEqual(options.parsed, True)
+        self.assertEqual(options.ner, True)
+        self.assertEqual(options.output_dir, "DIR")
+        self.assertEqual(args, ["out.txt"])
+
+    def test_single_commands(self):
+        _validate_args(["--tokenized", "--output=DIR", "out.txt"])
+        _validate_args(["--tagged", "--output=DIR", "out.txt"])
+        _validate_args(["--tagged", "--lemmatized", "--output=DIR", "out.txt"])
+        _validate_args(["--parsed", "--output=DIR", "out.txt"])
+        _validate_args(["--ner", "--tagged", "--lemmatized", "--output=DIR", "out.txt"])
+
+    def test_multiple_filenames(self):
+        options, args = _validate_args(["--tokenized", "--output=DIR", "out1.txt", "out2.txt"])
+        self.assertEqual(args, ["out1.txt", "out2.txt"])
+
+    def test_custom_models(self):
+        _validate_args(["--tagged", "--output=DIR", "--tagging-model=" + TAGGING_MODEL, "out.txt"])
+        _validate_args(["--tagged", "--lemmatized", "--output=DIR", "--lemmatization-model=" + LEMMATIZATION_MODEL, "out.txt"])
+        _validate_args(["--parsed", "--output=DIR", "--parsing-model=" + PARSING_MODEL, "out.txt"])
+        _validate_args(["--ner", "--tagged", "--lemmatized", "--output=DIR", "--ner-model=" + NER_MODEL, "out.txt"])
+
+    def test_custom_malt_jar(self):
+        _validate_args(["--tagged", "--output=DIR", "--malt=" + MALT, "out.txt"])
