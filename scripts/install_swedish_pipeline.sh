304eac4545a5894a1cbf97b92ac326f751a6bba4
Robert Östling <robert@ling.su.se>
2023-09-04 09:44:46 +0200

    Updated URL to model files

--- scripts/install_swedish_pipeline.sh
+++ scripts/install_swedish_pipeline.sh
@@ -5,7 +5,7 @@ make
 
 # Download and build models
 if [ ! -e swe-pipeline-ud2.tar.gz ]; then
-    wget http://mumin.ling.su.se/projects/efselab/swe-pipeline-ud2.tar.gz
+    wget https://dali.ling.su.se/projects/efselab/swe-pipeline-ud2.tar.gz
     tar xvzf swe-pipeline-ud2.tar.gz
     python3 build_suc.py --skip-generate --python --n-train-fields 2
     python3 build_suc_ne.py --skip-generate --python --n-train-fields 4
