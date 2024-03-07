aca938d9f7a22afbf76fb8997ea3ed0be0f9dfa7
skogsgren <6107181+skogsgren@users.noreply.github.com>
2024-03-07 14:09:43 +0100

    remove unused scripts and files used in original efselab

--- pefselab/scripts/test_lemmatizer.py
+++
@@ -1,7 +0,0 @@
-import lemmatize
-import sys
-
-lemmatizer = lemmatize.SUCLemmatizer()
-lemmatizer.load("suc-saldo.lemmas")
-
-print("%.2f%%" % (100.0 * lemmatizer.evaluate(sys.argv[1])))
--- pefselab/scripts/train_lemmatizer.py
+++
@@ -1,5 +0,0 @@
-import lemmatize
-import sys
-
-lemmatizer = lemmatize.train([sys.argv[1]], [sys.argv[2]])
-lemmatizer.save("suc-saldo.lemmas")
